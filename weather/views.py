4028f5ffe451b92a04a6ec4f31132c81ea693c3d
mavin321 <mavinpeteromondi@gmail.com>
2025-03-04 23:06:21 +0300

    V1

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method== 'POST':
+        city=request.POST['city']
+        data=urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=1837ef6a8ed195cc15e31f7f3176b2c7').read()
+        json_data=json.loads(data)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' +
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city': city, 'data': data})
